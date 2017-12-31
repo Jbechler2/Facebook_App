6f6fc6a78e22f741a79f75d1d54c1c30e8d338b0
Jbechler2 <jbechler2@gmail.com>
2017-12-31 15:37:44 -0800

    Began adding photo previewing functionality

--- Final_Design_1/Photos.xaml.cs
+++ Final_Design_1/Photos.xaml.cs
@@ -22,6 +22,13 @@ namespace Final_Design_1
     /// </summary>
     public partial class Photos : UserControl
     {
+        public dynamic albums;
+        public List<dynamic> album_photos = new List<dynamic>();
+        public List<dynamic> photo_srcs = new List<dynamic>();
+        static bool got_photos = false;
+        static public int album_cntr = 0;
+        static public int album1_cntr = 0;
+        static public int album2_cntr = 0;
 
         public dynamic fb_client()
         {
@@ -38,26 +45,86 @@ namespace Final_Design_1
 
             image_object.Source = bitmap;
         }
+        
+        private void set_text(dynamic text_object, string text)
+        {
+            text_object.Text = text;
+        }
+
+        private void hide(dynamic visual_object)
+        {
+            visual_object.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void show(dynamic visual_object)
+        {
+            visual_object.Visibility = System.Windows.Visibility.Visible;
+        }
 
         public Photos()
         {
             InitializeComponent();
-
-            get_album_covers();
+            album_cntr = 0;
+            get_albums();
+            preview_albums();
         }
 
-        private void get_album_covers()
+        private void get_albums()
         {
             var fb = fb_client();
+            albums = fb.Get("/me/albums?fields=name");
 
-            dynamic result = fb.Get("/me/albums?fields=cover_photo.width(1000).height(1000),source,name");
+            get_photos(fb);
+        }
 
-            dynamic album = fb.Get("/" + result.data[0].id.ToString() + "?fields=name,picture.type(album),photos&fields=source");
+        private void get_photos(dynamic fb)
+        {
 
-            set_image(album1_cover, album.picture.data.url);
-            album1_title.Text = result.data[0].name;
+            for (int i = 0; i < albums.data.Count; i++)
+            {
+                album_photos.Add(fb.Get("/" + albums.data[i].id + "/photos?fields=source,name"));
+            }
+            got_photos = true;
+        }
 
-            text1.Text = result.ToString();
+        private void preview_albums()
+        {
+            bool album1_set = false;
+            bool album2_set = false;
+
+            while (album_cntr < albums.data.Count && (!album1_set || !album2_set))
+            {
+                if (!album1_set)
+                {
+                    set_album1(albums.data[album_cntr], album_photos[album_cntr]);
+                    album1_cntr = album_cntr++;
+                    album1_set = true;
+                }
+
+                if (album_cntr > albums.data.Count)
+                    continue;
+
+                if (!album2_set)
+                {
+                    set_album2(albums.data[album_cntr], album_photos[album_cntr]);
+                    album2_cntr = album_cntr++;
+                    album2_set = true;
+                }
+            }
+            
+        }
+
+        private void set_album1(dynamic album, dynamic photos)
+        {
+            set_text(album1_title, album.name);
+            set_image(album1_cover, photos.data[0].source);
+        }
+
+        private void set_album2(dynamic album, dynamic photos)
+        {
+            set_text(text1, album.ToString());
+            set_text(album2_title, album.name);
+            set_image(album2_cover, photos.data[0].source);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -68,12 +135,60 @@ namespace Final_Design_1
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+           
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void expand1_Click(object sender, RoutedEventArgs e)
+        {
+            hide(next);
+            hide(prev);
+            hide(expand2);
+            hide(album2);
+            expand1.Content = "Minimize";
+
+            expand1.Click -= expand1_Click;
+            expand1.Click += minimize_Click_1;
+        }
+
+        private void expand2_Click(object sender, RoutedEventArgs e)
+        {
+            hide(next);
+            hide(prev);
+            hide(expand1);
+            hide(album1);
+            expand2.Content = "Minimize";
+
+            expand2.Click -= expand2_Click;
+            expand2.Click += minimize_Click_2;
+        }
+
+        private void minimize_Click_1(object sender, RoutedEventArgs e)
+        {
+            show(next);
+            show(prev);
+            show(expand2);
+            show(album2);
+            expand1.Content = "Expand";
+
+            expand1.Click -= minimize_Click_1;
+            expand1.Click += expand1_Click;
+        }
+
+        private void minimize_Click_2(object sender, RoutedEventArgs e)
+        {
+            show(next);
+            show(prev);
+            show(expand1);
+            show(album1);
+            expand2.Content = "Expand";
+
+            expand2.Click -= minimize_Click_2;
+            expand2.Click += expand2_Click;
         }
     }
 }
